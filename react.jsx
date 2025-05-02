36b969130d55d6c7adbfbb3f94f7d886369f1097
pari2404 <paribindal24@gmail.com>
2025-05-02 13:03:04 +0530

    REACT
    INTODUCTION

---
+++ react.jsx
@@ -0,0 +1,19 @@
+// react is a library,not a framework
+// a library im programmimg as collection of codes.simpler to write.import a feature frim it into our project
+// framework has its own library and rules,dont have much felexibility,eq=angular
+// dom-document object model
+// benefit of virtual dom---each time you make a change in code dom will be completely updated and rewritten.
+// /thus approach makes a webpage much fater than a standard webpage.
+
+// REACT CORE SYNTAX: JSX
+// const element= <h1>hello</h1>;
+
+// class Hello extends React.Component{
+//     render(){
+//         return <div> Hello {this.props.toWhat}</div>;
+//     }
+// }
+// ReactDOM.render{
+//     <hello toWhat="world"/>,
+//     document.getElementById('root')
+// };
